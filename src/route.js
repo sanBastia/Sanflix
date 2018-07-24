df0c88c412f6ec8fc7ebf8a983c0b47fdb973639
Irsan Sebastian <irsansebastianc4@gmail.com>
2018-07-24 16:17:55 +0700

    TOKOFLIX: adding new route to Movie detail

--- src/route.js
+++ src/route.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Detail from './pages/Detail';
+import Navigation from './modules/Navigation';
 
 
 // eslint-disable-next-line
@@ -9,8 +11,10 @@ class Routes extends React.Component {
     return (
       <Router>
         <div style={{ height: '100%' }}>
+          <Navigation />
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/:movie" component={Detail} />
           </Switch>
         </div>
       </Router>
