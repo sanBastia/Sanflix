2cf06973ed7d859413d50a157041cd3507a211d2
Irsan Sebastian <irsansebastianc4@gmail.com>
2018-07-24 19:22:16 +0700

    TOKOFLIX: adding the movieDetail reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import nowPlayingReducer from '../modules/MovieList/reducer';
+import movieDetailReducer from '../modules/MovieDetail/reducer';
 
 const rootReducer = combineReducers({
   nowPlayingReducer,
+  movieDetailReducer,
 });
 
 export default rootReducer;
