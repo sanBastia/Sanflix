b5ae9fa61efd158846d1f5d96de8a2213697abd7
Irsan Sebastian <irsansebastianc4@gmail.com>
2018-07-24 16:17:26 +0700

    TOKOFLIX: removing the navigation from this component, to route

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import MovieList from '../modules/MovieList';
-import Navigation from '../modules/Navigation';
 
 const Home = () => (
   <div>
-    <Navigation />
     <MovieList />
   </div>
 );
