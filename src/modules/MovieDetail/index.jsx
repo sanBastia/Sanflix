1face6e25dba1acaa92be7cd720869ced7351cf1
Irsan Sebastian <irsansebastianc4@gmail.com>
2018-07-25 14:21:28 +0700

    TOKOFLIX: styling a little bit, and also new component, button for purchase

--- src/modules/MovieDetail/index.jsx
+++ src/modules/MovieDetail/index.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
-  Jumbotron, Button, Badge, Row, Col, Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle,
+  Jumbotron, Badge, Row, Col, Card, CardImg, CardBody,
+  CardTitle, CardSubtitle, Button,
 } from 'reactstrap';
 import PropTypes from 'prop-types';
+import { FaTag, FaMoney } from 'react-icons/lib/fa/';
+import { TiStarFullOutline } from 'react-icons/lib/ti';
 
 import './style.css';
 import { requestMovieDetail, requestMovieCast } from './action';
 import LoadingBar from '../LoadingBar';
-import pic from '../../common/Images/profile.png';
+import person from '../../common/Images/profile.png';
+import movie from '../../common/Images/Movie.jpg';
+import { GetPrices } from './util';
 
-
-// eslint-disable-next-line
 class MovieDetail extends Component {
-
   static propTypes = {
     requestMovieDetail: PropTypes.func.isRequired,
     requestMovieCast: PropTypes.func.isRequired,
@@ -40,6 +41,27 @@ class MovieDetail extends Component {
     ) : overview
   )
 
+  renderTagline = (tagline) => {
+    if (tagline === '') {
+      return (
+        <div>
+          <FaTag size={30} />
+          <Badge className="miniTag" color="info">
+          Coming soon
+          </Badge>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <FaTag size={30} />
+        <i className="miniTag">
+          {tagline}
+        </i>
+      </div>
+    );
+  };
+
   renderCast = () => {
     const { movieCastReducer: { data: { cast } } } = this.props;
     if (cast) {
@@ -48,7 +70,7 @@ class MovieDetail extends Component {
           { cast.map(item => (
             <Col key={item.id} sm="3">
               <Card>
-                <CardImg top width="100%" src={item.profile_path === null ? pic : `https://image.tmdb.org/t/p/w500/${item.profile_path}`} alt="Card image cap" />
+                <CardImg top width="100%" src={item.profile_path === null ? person : `https://image.tmdb.org/t/p/w500/${item.profile_path}`} alt="Card image cap" />
                 <CardBody>
                   <CardTitle>
                     {item.character}
@@ -67,10 +89,37 @@ class MovieDetail extends Component {
     return 'Loading';
   }
 
+  renderRating = rate => (
+    <div>
+      <TiStarFullOutline size={30} />
+      <span className="miniTag">
+        {rate}
+      </span>
+    </div>
+  )
+
+  renderPrice = rate => (
+    <div>
+      <FaMoney size={30} />
+      <span className="miniTag">
+        {GetPrices(rate)}
+      </span>
+    </div>
+  )
+
+  renderPurchaseButton = rate => (
+    <Button className="miniInfo" color="success" size="lg" block disabled={rate === 0}>
+      {rate === 0 ? 'Coming Soon' : 'PURCHASE'}
+    </Button>
+  )
+
+
   render() {
-    console.log(this.props, 'this props');
     const { movieDetailReducer: { data, activeRequests } } = this.props;
-    const { renderOverview, renderCast } = this;
+    const {
+      renderOverview, renderCast, renderTagline,
+      renderRating, renderPrice, renderPurchaseButton,
+    } = this;
 
     if (data && activeRequests === 0) {
       return (
@@ -81,13 +130,26 @@ class MovieDetail extends Component {
                 <h1 className="display-3">
                   {data.title}
                 </h1>
-                <p className="lead">
-                  {data.tagline}
-                </p>
+                <Row>
+                  <Col className="miniInfo" sm="6">
+                    {renderTagline(data.tagline)}
+                  </Col>
+                  <Col className="miniInfo" sm="6">
+                    {renderRating(data.vote_average)}
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="miniInfo">
+                    {renderPrice(data.vote_average)}
+                  </Col>
+                </Row>
+                <Row>
+                  {renderPurchaseButton(data.vote_average)}
+                </Row>
               </Col>
               <Col md="6">
                 <Card>
-                  <CardImg top width="100%" src={`https://image.tmdb.org/t/p/w500/${data.backdrop_path}`} alt="Card image cap" />
+                  <CardImg top width="100%" src={data.backdrop_path === null ? movie : `https://image.tmdb.org/t/p/w500/${data.backdrop_path}`} alt="Card image cap" />
                 </Card>
               </Col>
             </Row>
